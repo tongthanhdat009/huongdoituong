eaa308b8735c52472583fbd680b78e4fcd14b493
LuuD04N <121650129+LuuD04N@users.noreply.github.com>
2023-12-03 17:19:40 +0700

    Update diem.java

--- diem.java
+++ diem.java
@@ -1,24 +1,21 @@
-// Lớp điểm (diem)
 public class diem {
-    // Các thuộc tính của lớp điểm
-    private float avgDiem; // Điểm trung bình
-    private String xeploai; // Xếp loại học sinh
-    private int namhoc; // Năm học
-    private int hocky; // Học kỳ
-    private monhoc[] monhoc; // Mảng các môn học
-    private int maHS; // Mã học sinh
 
-    // Phương thức khởi tạo lớp điểm
+    private float avgDiem;
+    private String xeploai;
+    private int namhoc;
+    private int hocky;
+    private monhoc[] monhoc;
+    private int maHS;
+
     public diem(int maHS, int namhoc, int hocky, monhoc[] monhoc) {
         this.maHS = maHS;
         this.namhoc = namhoc;
         this.hocky = hocky;
         this.monhoc = monhoc;
-        this.avgDiem = tinhDiemTB(); // Tính điểm trung bình
-        this.xeploai = setXeploai(); // Đặt xếp loại học sinh
+        this.avgDiem = tinhDiemTB();
+        this.xeploai = setXeploai();
     }
 
-    // Các phương thức get và set cho các thuộc tính
     public float getAvgDiem() {
         return avgDiem;
     }
@@ -67,30 +64,28 @@ public class diem {
         this.maHS = maHS;
     }
 
-    // Phương thức tính điểm trung bình
     public float tinhDiemTB() {
-        float tongdiem = 0; // Tổng điểm của các môn học
-        int somon = monhoc.length; // Số môn học
+        float tongdiem = 0;
+        int somon = monhoc.length;
         for (int i = 0; i < somon; i++) {
-            tongdiem += monhoc[i].getDiem(); // Cộng dồn điểm của từng môn học
+            tongdiem += monhoc[i].getDiem();
         }
-        return tongdiem / somon; // Trả về điểm trung bình
+        return tongdiem / somon;
     }
 
-    // Phương thức đặt xếp loại học sinh
     public String setXeploai() {
         if (avgDiem >= 9) {
-            return "Xuat sac"; // Xếp loại xuất sắc nếu điểm trung bình từ 9 trở lên
+            return "Xuat sac";
         } else if (avgDiem >= 8) {
-            return "Gioi"; // Xếp loại giỏi nếu điểm trung bình từ 8 đến dưới 9
+            return "Gioi";
         } else if (avgDiem >= 6.5) {
-            return "Kha"; // Xếp loại khá nếu điểm trung bình từ 7 đến dưới 8
+            return "Kha";
         } else if (avgDiem >= 5) {
-            return "Trung binh"; // Xếp loại trung bình nếu điểm trung bình từ 6 đến dưới 7
+            return "Trung binh";
         } else if (avgDiem >= 3.5) {
-            return "Yeu"; // Xếp loại yếu nếu điểm trung bình từ 5 đến dưới 6
+            return "Yeu";
         } else {
-            return "Kem"; // Xếp loại kém nếu điểm trung bình dưới 5
+            return "Kem";
         }
     }
 }
